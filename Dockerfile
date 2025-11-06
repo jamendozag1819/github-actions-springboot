89084e6a937eb4da58d1522707daad4e9043b0b2
Mendoza Garcia <jamendozag@minsait.com>
2025-11-06 16:41:49 -0600

    first commit

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM rockylinux
+EXPOSE 8080
+ADD target/api-springboot-0.0.1.jar api-springboot-new.jar
+ENTRYPOINT [ "java", "-jar","/api-springboot-new.jar" ]
---
+++ src/main/java/com/github_actions/ApiSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.github_actions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringbootApplication.class, args);
+	}
+
+}
