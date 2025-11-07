e5ecb6fc5732e5637f07cace70b1f99f67665f9c
Mendoza Garcia <jamendozag@minsait.com>
2025-11-06 20:02:35 -0600

    Cambios junit

---
+++ src/test/java/com/github_actions/controller/ControllerTest.java
@@ -0,0 +1,23 @@
+package com.github_actions.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(Controller.class)
+public class ControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testMensajeEndpoint() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Prueba controller"));
+    }
+}
