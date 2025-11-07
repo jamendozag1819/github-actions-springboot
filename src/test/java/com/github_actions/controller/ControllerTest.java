10bee5c426c2a2e787a1ecd90d991a17a7701b7b
Mendoza Garcia <jamendozag@minsait.com>
2025-11-07 17:01:28 -0600

    Cambios CI/CD

--- src/test/java/com/github_actions/controller/ControllerTest.java
+++ src/test/java/com/github_actions/controller/ControllerTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -17,5 +18,6 @@ public class ControllerTest {
 	@Test
 	public void testMensajeEndpoint() throws Exception {
 		mockMvc.perform(get("/")).andExpect(status().isOk()).andExpect(content().string("Prueba controller"));
+		assertTrue(true);
 	}
 }
