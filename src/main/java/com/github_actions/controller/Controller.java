7ece1bbd2e1f31eabe175049d10222b9e01bb193
Mendoza Garcia <jamendozag@minsait.com>
2025-11-13 18:18:36 -0600

    Cambios CI/DI

--- src/main/java/com/github_actions/controller/Controller.java
+++ src/main/java/com/github_actions/controller/Controller.java
@@ -11,23 +11,24 @@ public class Controller {
 	public String mensaje() {
 		return "Prueba controller";
 	}
-	
+
 	@GetMapping("/vuln")
-	public String insecure(@RequestParam String input){
-	    return "Result: " + input; // posible XSS
+	public String insecure(@RequestParam String input) {
+		return "Result: " + input; // posible XSS
 	}
 
 	@GetMapping("/run")
 	public String run(@RequestParam String cmd) throws Exception {
-	    Runtime.getRuntime().exec(cmd); // crítico
+		Runtime.getRuntime().exec(cmd); // crítico
+		return cmd;
 	}
-	
+
 	@GetMapping("/no-test")
 	public String noTestMethod() {
 		int unused = 0;
 		String text = "hello";
 
-	    return "no test";
+		return "no test";
 	}
 
 	public void hugeMethod() {
@@ -36,36 +37,35 @@ public class Controller {
 		// código muerto
 		// int x = 10;
 
-		
-		
-	    for(int i=0; i<100; i++){
-	        System.out.println(i);
-	    }
-	    // copia el loop varias veces
+		for (int i = 0; i < 100; i++) {
+			System.out.println(i);
+		}
+		// copia el loop varias veces
 	}
 
-	
-	public int complex(int x){
-	    if(x==1) return 1;
-	    if(x==2) return 2;
-	    return 0;
+	public int complex(int x) {
+		if (x == 1)
+			return 1;
+		if (x == 2)
+			return 2;
+		return 0;
 	}
 
-	
-	public int fail(int x){
-	    return x / (x - x);  // posible división por cero
+	public int fail(int x) {
+		return x / (x - x); // posible división por cero
 	}
 
-	
-	public String npe(String s){
-	    return s.toString();
+	public String npe(String s) {
+		return s.toString();
 	}
 
 	public void arrayBug() {
-	    int[] a = new int[2];
-	    int x = a[5]; // index out of bounds
-	}
+		int[] a = new int[2];
+		int x = a[5]; // index out of bounds
+		String name = "Javier";
+		if (name == "test") {
 
-	if(name == "test") { ... }
+		}
+	}
 
 }
