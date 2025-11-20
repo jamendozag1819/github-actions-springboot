a3cc1f882a54c853c799e5e3fd5c11e0d7474d32
Javier Alexis <jamendozag@minsait.com>
2025-11-20 15:44:26 -0600

    Cambios cicd

--- scripts/snyk-command-map.py
+++ scripts/snyk-command-map.py
@@ -7,7 +7,7 @@ stack = os.getenv("SNYK_STACK", "unknown")
 print(f"Resolving Snyk command for stack: {stack}")
 
 SNYK_COMMAND_MAP = {
-    "java-maven": "snyk test --file=pom.xml --package-manager=maven",
+    "java-maven": "snyk test --all-projects",
     "java-gradle": "snyk test --all-projects",
     "android": "snyk test --all-projects",
     "ios": "snyk test --all-projects",
