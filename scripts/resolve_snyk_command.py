371f82eae09878dca8393d35ca7224d95ba9bd40
Javier Alexis <jamendozag@minsait.com>
2025-11-26 17:24:07 -0600

    Prueba cicd

--- scripts/resolve_snyk_command.py
+++ scripts/resolve_snyk_command.py
@@ -14,7 +14,7 @@ SNYK_COMMAND_MAP = {
     "angular": "snyk test --all-projects",
     "nodejs": "snyk test",
     "python": "snyk test",
-    "docker": "snyk test --docker Dockerfile"
+    "docker": "snyk container test"
 }
 
 DEFAULT_CMD = "snyk test"
