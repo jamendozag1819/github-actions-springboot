b72377bee18c06056903b28f1bf55c04b82ce013
Javier Alexis <jamendozag@minsait.com>
2025-11-27 23:24:08 -0600

    19 implementacion de compuertas jira

--- scripts/sonar_gating.py
+++ scripts/sonar_gating.py
@@ -47,7 +47,7 @@ def fetch_json(url, user=None, token=None, is_jira=False, jql=None ,body= None):
             auth = base64.b64encode(f"{user}:{token}".encode()).decode()
         else:
             auth = base64.b64encode(f"{token}:".encode()).decode()
-		print(f"Authorization : ",auth)
+        print(f"Authorization : ",auth)
         req.add_header("Authorization", f"Basic {auth}")
 
         with urllib.request.urlopen(req, timeout=30) as response:
